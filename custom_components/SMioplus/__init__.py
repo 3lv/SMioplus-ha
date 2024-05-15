61c7124b33be1098b17f9baa011162a9e0c5ef1e
3lv <cristian.vlad.vladutu@gmail.com>
2024-05-15 20:28:30 +0300

    First commit

---
+++ custom_components/SMioplus/__init__.py
@@ -0,0 +1,105 @@
+"""Sequent Microsystems Home Automation Integration"""
+
+import logging
+import voluptuous as vol
+
+from homeassistant.helpers import config_validation as cv
+from homeassistant.const import (
+	CONF_NAME
+)
+CONF_NAME = CONF_NAME
+CONF_STACK = "stack"
+CONF_TYPE = "type"
+CONF_CHAN = "chan"
+
+DOMAIN = "SMioplus"
+NAME_PREFIX = "smio"
+SM_MAP = {
+    "sensor":  {
+        "opto": {
+                "chan_no": 8,
+                "uom": "",
+                "com": {
+                    "get": "getOptoCh",
+                },
+                "icon": {
+                    "on": "mdi:mdi-numeric",
+                    "off": "mdi:mdi-numeric"
+                }
+        },
+    },
+    "switch": {
+        "relay": {
+                "chan_no": 8,
+                "com": {
+                    "get": "getRelayCh",
+                    "set": "setRelayCh"
+                },
+                "icon": {
+                    "on": "mdi:toggle-switch-variant",
+                    "off": "mdi:toggle-switch-variant-off",
+                }
+        }
+    },
+}
+
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema(vol.Any([vol.Schema({
+        vol.Optional(CONF_STACK, default="0"): cv.string,
+    }, extra=vol.ALLOW_EXTRA)], {}))
+}, extra=vol.ALLOW_EXTRA)
+
+_LOGGER = logging.getLogger(__name__)
+
+def load_platform(hass, entity_config):
+        for platform_type, attr in SM_MAP.items():
+            if entity_config[CONF_TYPE] in attr:
+                hass.helpers.discovery.load_platform(
+                        platform_type, DOMAIN, entity_config, entity_config
+                )
+def load_all_platforms(hass, stack=0):
+    for platform_type, platform in SM_MAP.items():
+        for type, attr in platform.items():
+            if attr.get("optional", False):
+                continue
+            for chan in range(int(attr["chan_no"])):
+                entity_config = {
+                        CONF_NAME: NAME_PREFIX+str(stack)+"_"+type+"_"+str(chan+1),
+                        CONF_STACK: stack,
+                        CONF_TYPE: type,
+                        CONF_CHAN: chan+1
+                }
+                hass.helpers.discovery.load_platform(
+                        platform_type, DOMAIN, entity_config, {}
+                )
+
+
+def setup(hass, config):
+    hass.data[DOMAIN] = []
+    card_configs = config.get(DOMAIN)
+    if not card_configs:
+        load_all_platforms(hass, stack=0)
+        return True
+    for card_config in card_configs:
+        stack = int(card_config.pop(CONF_STACK, 0))
+        if not card_config:
+            load_all_platforms(hass, stack=stack)
+            continue
+        for entity in card_config:
+            try:
+                [type, chan] = entity.rsplit("_", 1)
+                chan = int(chan)
+            except:
+                _LOGGER.error(entity, " doesn't respect type-chan format")
+                continue
+            entity_config = card_config[entity] or {}
+            entity_config |= {
+                    CONF_NAME: NAME_PREFIX + str(stack) + "_" + entity,
+                    CONF_STACK: stack,
+                    CONF_TYPE: type,
+                    CONF_CHAN: chan
+            }
+            load_platform(hass, entity_config)
+        
+    return True
---
+++ custom_components/SMioplus/sensor.py
@@ -0,0 +1,93 @@
+import voluptuous as vol
+import logging
+import time
+import types
+import inspect
+from inspect import signature
+
+import libioplus as SMioplus
+
+from homeassistant.components.light import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.entity import generate_entity_id
+
+from . import (
+        DOMAIN, CONF_STACK, CONF_TYPE, CONF_CHAN, CONF_NAME,
+        NAME_PREFIX,
+        SM_MAP
+)
+SM_SENSOR_MAP = SM_MAP["sensor"]
+
+#SCHEMA_EXTEND = {
+#	vol.Optional(CONF_NAME, default=""): cv.string,
+#	vol.Optional(CONF_STACK, default="0"): cv.string,
+#}
+#for key in SM_SENSOR_MAP:
+#    SCHEMA_EXTEND[vol.Optional(key, default="-1")] = cv.string
+#PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(SCHEMA_EXTEND)
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    # We want this platform to be setup via discovery
+    if discovery_info == None:
+        return
+    add_devices([Sensor(
+		name=discovery_info.get(CONF_NAME, ""),
+        stack=discovery_info.get(CONF_STACK, 0),
+        type=discovery_info.get(CONF_TYPE),
+        chan=discovery_info.get(CONF_CHAN),
+        hass=hass
+	)])
+
+class Sensor(SensorEntity):
+    """Sequent Microsystems SMioplus Sensor"""
+    def __init__(self, name, stack, type, chan, hass):
+        generated_name = DOMAIN + str(stack) + "_" + type + "_" + str(chan)
+        self._unique_id = generate_entity_id("sensor.{}", generated_name, hass=hass)
+        self._name = name or generated_name
+        self._stack = int(stack)
+        self._type = type
+        self._chan = int(chan)
+        self._SM = SMioplus
+        # Altering class so alln functions have the same format
+        com = SM_SENSOR_MAP[self._type]["com"]
+        self._short_timeout = .05
+        self._icons = SM_SENSOR_MAP[self._type]["icon"]
+        self._icon = self._icons["off"]
+        self._uom = SM_SENSOR_MAP[self._type]["uom"]
+        self._value = 0
+        self._SM_get = getattr(self._SM, com["get"])
+        if len(signature(self._SM_get).parameters) == 0:
+            def _aux_SM_get(self, _):
+                return getattr(self, com["get"])()
+            self._SM_get = types.MethodType(_aux_SM_get, self._SM)
+
+    def update(self):
+        time.sleep(self._short_timeout)
+        try:
+            self._value = self._SM_get(self._chan)
+        except Exception as ex:
+            _LOGGER.error(DOMAIN + " %s update() failed, %e, %s, %s", self._type, ex, str(self._stack), str(self._chan))
+            return
+        if self._value != 0:
+            self._icon = self._icons["on"]
+        else:
+            self._icon = self._icons["off"]
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return self._icon
+
+    @property
+    def native_unit_of_measurement(self):
+        return self._uom
+
+    @property
+    def native_value(self):
+        return self._value
