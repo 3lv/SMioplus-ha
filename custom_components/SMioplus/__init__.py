04256840df0de263533957247e6c10c2738cfc0d
3lv <cristian.vlad.vladutu@gmail.com>
2024-08-09 01:01:10 +0300

    test

--- custom_components/SMioplus/__init__.py
+++ custom_components/SMioplus/__init__.py
@@ -7,85 +7,18 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.const import (
 	CONF_NAME
 )
+
+from . import data
+DOMAIN = data.DOMAIN
+NAME_PREFIX = data.NAME_PREFIX
+SM_MAP = data.SM_MAP
+
 CONF_NAME = CONF_NAME
 CONF_STACK = "stack"
 CONF_TYPE = "type"
 CONF_CHAN = "chan"
 COM_NOGET = "__NOGET__"
 
-DOMAIN = "SMioplus"
-NAME_PREFIX = "smio"
-SM_MAP = {
-    "sensor":  {
-        "opto": {
-                "chan_no": 8,
-                "uom": "",
-                "com": {
-                    "get": "getOptoCh",
-                },
-                "icon": {
-                    "on": "mdi:mdi-numeric",
-                    "off": "mdi:mdi-numeric"
-                }
-        },
-        "adc": {
-                "chan_no": 8,
-                "uom": "V",
-                "com": {
-                    "get": "getAdcV",
-                },
-                "icon": {
-                    "on": "mdi:flash-triangle",
-                    "off": "mdi:flash-triangle"
-                }
-        },
-    },
-    "switch": {
-        "relay": {
-                "chan_no": 8,
-                "com": {
-                    "get": "getRelayCh",
-                    "set": "setRelayCh"
-                },
-                "icon": {
-                    "on": "mdi:toggle-switch-variant",
-                    "off": "mdi:toggle-switch-variant-off",
-                }
-        }
-    },
-    "number": {
-        "dac": {
-                "chan_no": 4,
-                "uom": "V",
-                "min_value": 0.0,
-                "max_value": 10.0,
-                "step": 0.01,
-                "com": {
-                    "get": "__NOGET__", # TODO: CHANGE THIS TEMPORARY SOLUTION
-                    "set": "setDacV"
-                },
-                "icon": {
-                    "on": "mdi:flash-triangle",
-                    "off": "mdi:flash-triangle"
-                }
-        },
-        "od": {
-                "chan_no": 4,
-                "uom": "%",
-                "min_value": 0.0,
-                "max_value": 100.0,
-                "step": 0.01,
-                "com": {
-                    "get": "__NOGET__", # TODO: CHANGE THIS TEMPORARY SOLUTION
-                    "set": "setOdPwm"
-                },
-                "icon": {
-                    "on": "mdi:flash-triangle",
-                    "off": "mdi:flash-triangle"
-                }
-        },
-    },
-}
 
 
 CONFIG_SCHEMA = vol.Schema({
